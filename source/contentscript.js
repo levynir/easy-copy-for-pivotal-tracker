a9555c896349dae7b60b32d555c76e53425de1e2
fukayatsu <fukayatsu@gmail.com>
2012-10-12 21:42:43 +0900

    support more keys!
    now support following keys:
    
      Alt          #=>  id
      Shift        #=>  link
      Meta         #=>  name
      Meta + Alt   #=>  name #id
      Meta + Shift #=> [name](link)

--- source/contentscript.js
+++ source/contentscript.js
@@ -2,21 +2,27 @@ chrome.extension.sendRequest({'action': 'pageStart'});
 
 $(document).ready(function(){
   $('.item').live('click', function(e){
-    var storyId = $(this).attr('id').replace(/.*?(\d+).*/,"$1");
-    var clip = {};
+    var id = $(this).attr('id').replace(/.*?(\d+).*/,"$1");
+    var name = $(this).find('.story_name').text();
+    var link = "https://www.pivotaltracker.com/story/show/" + id;
 
-    if (e.shiftKey) {
-      clip.alert    = "Copied id: ";
-      clip.textData = storyId;
+    var clip;
+    if (e.metaKey && e.shiftKey) {
+      clip = "[" + name + "](" + link + ")";
+    } else if (e.metaKey && e.altKey) {
+      clip = name + " #" + id;
+    } else if (e.altKey) {
+      clip = id;
+    } else if (e.shiftKey) {
+      clip = link;
     } else if (e.metaKey) {
-      clip.alert    = "Copied link: ";
-      clip.textData = "https://www.pivotaltracker.com/story/show/" + storyId;
+      clip = name;
     }
 
-    if (clip.textData) {
+    if (clip) {
       $('<div id="copy-msg">')
         .css({ color: 'black' })
-        .html(clip.alert + clip.textData)
+        .html(clip)
         .prependTo('#status');
       $('#status')
         .fadeIn(10)
@@ -25,7 +31,7 @@ $(document).ready(function(){
           $('#copy-msg').remove();
         });
 
-      chrome.extension.sendRequest({'text' : clip.textData});
+      chrome.extension.sendRequest({'text' : clip});
     }
   });
 });
