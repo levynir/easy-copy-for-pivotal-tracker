385a3541496773000deeadfd9de5efe50e8773bd
fukayatsu <fukayatsu@gmail.com>
2012-10-12 21:27:08 +0900

    add metaKey to copy the story link

--- source/contentscript.js
+++ source/contentscript.js
@@ -2,13 +2,30 @@ chrome.extension.sendRequest({'action': 'pageStart'});
 
 $(document).ready(function(){
   $('.item').live('click', function(e){
-    if (e.shiftKey) {
-      var storyId = $(this).attr('id').replace(/.*?(\d+).*/,"$1");
-      var text    = "Copied: " + storyId;
+    var storyId = $(this).attr('id').replace(/.*?(\d+).*/,"$1");
+    var clip = {};
 
-      $('<div id="copy-msg">').css({ color: 'black' }).html(text).prependTo('#status');
-      $('#status').fadeIn(10).delay(1000).fadeOut(500, function() { $('#copy-msg').remove(); })
-      chrome.extension.sendRequest({'text' : storyId});
+    if (e.shiftKey) {
+      clip.alert    = "Copied id: ";
+      clip.textData = storyId;
+    } else if (e.metaKey) {
+      clip.alert    = "Copied link: ";
+      clip.textData = "https://www.pivotaltracker.com/story/show/" + storyId;
+    }
+
+    if (clip.textData) {
+      $('<div id="copy-msg">')
+        .css({ color: 'black' })
+        .html(clip.alert + clip.textData)
+        .prependTo('#status');
+      $('#status')
+        .fadeIn(10)
+        .delay(1000)
+        .fadeOut(500, function() {
+          $('#copy-msg').remove();
+        });
+
+      chrome.extension.sendRequest({'text' : clip.textData});
     }
   });
 });
