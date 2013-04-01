e72818a4c61250d9e3c0ce6d4147ce14ef223d3a
fukayatsu <fukayatsu@gmail.com>
2013-04-01 20:26:07 +0900

    Use sendMessage/onMessage instead of sendRequest/onRequest

--- source/js/background.js
+++ source/js/background.js
@@ -1,4 +1,4 @@
-chrome.extension.onRequest.addListener(function (request, sender, callback) {
+chrome.extension.onMessage.addListener(function (request, sender, callback) {
   if(request.action == "pageStart") {
     chrome.pageAction.show(sender.tab.id);
   }
--- source/js/contentscript.js
+++ source/js/contentscript.js
@@ -1,37 +1,35 @@
-chrome.extension.sendRequest({'action': 'pageStart'});
+chrome.extension.sendMessage({'action': 'pageStart'});
 
-$(document).ready(function(){
-  $(document).on('click', '.item', function(e){
-    var id = $(this).attr('id').replace(/.*?(\d+).*/,"$1");
-    var name = $(this).find('.story_name').text();
-    var link = "https://www.pivotaltracker.com/story/show/" + id;
+$(document).on('click', '.item', function(e){
+  var id = $(this).attr('id').replace(/.*?(\d+).*/,"$1");
+  var name = $(this).find('.story_name').text();
+  var link = "https://www.pivotaltracker.com/story/show/" + id;
 
-    var clip;
-    if (e.metaKey && e.shiftKey) {
-      clip = "[" + name + "](" + link + ")";
-    } else if (e.metaKey && e.altKey) {
-      clip = name + " #" + id;
-    } else if (e.altKey) {
-      clip = id;
-    } else if (e.shiftKey) {
-      clip = link;
-    } else if (e.metaKey) {
-      clip = name;
-    }
+  var clip;
+  if (e.metaKey && e.shiftKey) {
+    clip = "[" + name + "](" + link + ")";
+  } else if (e.metaKey && e.altKey) {
+    clip = name + " #" + id;
+  } else if (e.altKey) {
+    clip = id;
+  } else if (e.shiftKey) {
+    clip = link;
+  } else if (e.metaKey) {
+    clip = name;
+  }
 
-    if (clip) {
-      $('<div id="copy-msg">')
-        .css({ color: 'black' })
-        .html(clip)
-        .prependTo('#status');
-      $('#status')
-        .fadeIn(10)
-        .delay(1000)
-        .fadeOut(500, function() {
-          $('#copy-msg').remove();
-        });
+  if (clip) {
+    $('<div id="copy-msg">')
+      .css({ color: 'black' })
+      .html(clip)
+      .prependTo('#status');
+    $('#status')
+      .fadeIn(10)
+      .delay(1000)
+      .fadeOut(500, function() {
+        $('#copy-msg').remove();
+      });
 
-      chrome.extension.sendRequest({'text' : clip});
-    }
-  });
+    chrome.extension.sendMessage({'text' : clip});
+  }
 });
