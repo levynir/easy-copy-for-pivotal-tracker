a89f9e3a9ec2bd10de2acfd709386d666aac237d
levynir <javajavaproxy@gmail.com>
2013-10-23 10:02:36 +0200

    fits fixes fiets fits fats sufises

--- source/js/contentscript.js
+++ source/js/contentscript.js
@@ -3,22 +3,21 @@ $(function() {
 });
 
 $(document).on('click', '.item', function(e){
-  var id = $(this).attr('id').replace(/.*?story(\d+).*/,"$1");
+  var id = $(this).attr('class').replace(/.*?story_(\d+).*/,"$1");
   var name = $(this).find('.story_name').text();
   var link = "https://www.pivotaltracker.com/story/show/" + id;
 
   var clip;
   if ( e.shiftKey) {
-    clip = "[" + id + "] " + name + "<br>(" + link + ")";
+    clip = "[#" + id + "] " + name + "\n(" + link + ")";
   }
   
   if (clip) {
     $('<div id="copy-msg">')
-      .css({ color: 'black' })
+      .css({ color: 'red' })
       .html(clip)
-      .prependTo('#status');
-    clip = $('#copy-msg').text(); //this is in order to change the BR to newline
-    $('#status')
+      .prependTo('.status');
+    $('.status')
       .fadeIn(10)
       .delay(1000)
       .fadeOut(500, function() {
